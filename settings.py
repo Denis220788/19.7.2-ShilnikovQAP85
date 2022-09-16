2abf928c2bb4c85b62d88ad0ad9f8082bad1109c
Denis220788 <107349520+Denis220788@users.noreply.github.com>
2022-09-16 19:27:25 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+valid_email = "shilnikov220788@gmail.com"
+valid_password = "Petfriends"
+
+invalid_email = "Shilnikov@mail.ru"
+invalid_password = "petfriends"
+
+empty_email = ""
+empty_password = ""
+
+invalid_auth_key = {'key' : '1234567890'}
+empty_auth_key = {'key' : ''}
